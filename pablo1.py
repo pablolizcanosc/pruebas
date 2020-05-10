6f0b1d5ed4cfe6e261d4aa860e8b95f0c072fcb9
Pablo Lizcano <pablod.lizcanosc@gmail.com>
2020-05-10 11:35:53 +0200

    pequena modifica

--- pablo1.py
+++ pablo1.py
@@ -1,3 +1,1 @@
-import pandas as pd
-pd.Series([0,5,7])
-[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[Df[B[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D
+#aqui no hay nah
